5eab6ae6953c4e7a7b1b2504b39f02a3171805d6
bunprodzai <132491110+bunprodzai@users.noreply.github.com>
2025-07-15 22:07:07 +0700

    fix bug mongo

--- config/database.js
+++ config/database.js
@@ -1,11 +1,47 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL;
+
+// // mongoose.set('debug', true);
+// // mongoose.connection.on('error', console.error);
+// // mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected'));
+// // mongoose.connection.on('connected', () => console.log('MongoDB connected'));
+
+// module.exports.connect = async () => {
+//   try {
+//     await mongoose.connect(MONGO_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       maxPoolSize: 10,
+//       serverSelectionTimeoutMS: 10000,
+//       socketTimeoutMS: 45000,
+//     });
+//     console.log("Connect susccess");
+//   } catch (error) {
+//     console.log("Connect failed");
+//   }
+// }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 module.exports.connect = async () => {
-  try {
-    await mongoose.connect(MONGO_URL);
-    console.log("Connect susccess");
-  } catch (error) {
-    console.log("Connect failed");
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URL, {
+      bufferCommands: false,
+      maxPoolSize: 10,
+    }).then((mongoose) => {
+      console.log('✅ MongoDB connected (singleton)');
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
